89d3b1dd942038151ec9d4a08138da790ae325ac
Aliza Usman <alizausman9@gmail.com>
2026-07-28 19:46:58 +0500

    erode and dilate

--- src/maskpipeline.py
+++ src/maskpipeline.py
@@ -12,10 +12,17 @@ while True:
 
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(hsv, LOWER_HSV, UPPER_HSV) #masks markers colour range
-    
-    
+
+    #opening
+    cleaned = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel) #removes noise
+    #closing
+    cleaned = cv2.morphologyEx(cleaned, cv2.MORPH_CLOSE, kernel) #fills holes
+
     cv2.imshow("Raw mask", mask)
     cv2.imshow("Cleaned mask", cleaned)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
+cap.release()
+cv2.destroyAllWindows()
