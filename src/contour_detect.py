accc6e7224c7800626c9c606e030b5b90f052ebc
Aliza Usman <alizausman9@gmail.com>
2026-07-29 17:46:52 +0500

    contour

--- src/contour_detect.py
+++ src/contour_detect.py
@@ -10,4 +10,26 @@ while True:
     ret,frame =cap.read()
     if not ret:
         break
-    
+
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, LOWER_HSV, UPPER_HSV)
+    cleaned = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel) #removes noise
+    cleaned = cv2.morphologyEx(cleaned, cv2.MORPH_CLOSE, kernel) #fills the holes
+
+    contours, _= cv2.findContours(cleaned, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    if contours:
+        largest = max(contours, key=cv2.contourArea)
+        area = cv2.contourArea(largest)
+        if area > min_area:
+            x,y,w,h = cv2.boundingRect(largest)
+            cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 2)
+            cv2.putText(frame, f"Area: {area}", (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0), 2)
+
+        cv2.imshow("Contours", frame)
+        cv2.imshow("Cleaned mask", cleaned)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+cap.release()
+cv2.destroyAllWindows()
