579337b40ed8d527a75681533773d8e57cab35d3
Aliza Usman <alizausman9@gmail.com>
2026-07-31 00:19:13 +0500

    centre calc

--- src/centroid_tracker.py
+++ src/centroid_tracker.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-from marker_config import LOWER_HSV, UPPER_HSV
+from markerconfig import LOWER_HSV, UPPER_HSV
 
 cap = cv2.VideoCapture(0)
 kernel = np.ones((5, 5), np.uint8)
